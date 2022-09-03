2d1d19497a389bc553260fece2c9ef6df8d83fe5
Sashank Khadgi <sashank.steam1@gmail.com>
2022-09-03 13:13:57 +0545

    signin

--- signin.php
+++ signin.php
@@ -25,10 +25,30 @@
 	    }
 	}
 	else {
+		if($email == null && $password == null){
 		echo '<script>
-				alert("Invalid User Credential");	
+				alert("Please enter your email and password");	
 				window.location.href="login.php";
 			  </script>';
+		}
+		else if($email != null && $password == null){
+			echo '<script>
+				alert("Please enter your password");	
+				window.location.href="login.php";
+			  </script>';
+		}
+		else if($email == null && $password != null){
+			echo '<script>
+				alert("Please enter your email");	
+				window.location.href="login.php";
+			  </script>';
+		}
+		else{
+			echo '<script>
+				alert("Invalid user credentials");	
+				window.location.href="login.php";
+			  </script>';
+		}
 	}
 ?>
 </body>
